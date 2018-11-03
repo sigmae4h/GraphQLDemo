a80c916a2afc02612bf1b0f5cfe2bf3c719e2ac6
Sigma <sigma.e4h@gmail.com>
2018-11-03 13:49:29 -0400

    Initial commit

---
+++ src/main/java/com/example/GraphQLDemo/GraphQlDemoApplication.java
@@ -0,0 +1,14 @@
+package com.example.GraphQLDemo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class GraphQlDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphQlDemoApplication.class, args);
+	}
+}
